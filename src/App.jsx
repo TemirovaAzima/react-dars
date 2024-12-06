45edf024a4315f2681d37cfca215274b2406158b
TemirovaAzima <temirovaazima9@gmail.com>
2024-12-07 01:54:09 +0500

    Props end

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,37 @@
-import {UserList} from "./components/UserList.jsx";
+// import React from 'react'
+//
+// const App = () => {
+//     return (
+//         <main>
+//             <User name ='Azima' age={17} hobbies={["Coding ","Dancing"]} userss={{name:"kik", age: 23}} little={[{name
+//             :'jjj', id: 1}, {name:'lll', id: 2}]}/>
+//         </main>
+//     )
+// }
+// export default App
+// const User = ({name, age, hobbies, little})=> {
+//     console.log(name)
+//     return (
+//         <main>
+//             <h1>My name is {name}</h1>
+//             <h1>My age is {age}</h1>
+//             <h1> My hobbies:{hobbies}</h1>
+//             {/*<p>{userss.name}</p>*/}
+//             {/*<p>{userss.age}</p>*/}
+//             {little.map((item, index) => (
+//                 <ul key = {index}>
+//                     <li key={Math.random()}>Name: {item.name} id : {item.id}</li>
+//                 </ul>
+//             ))}
+//         </main>
+//     )
+// }
+
+import Person from './components/Person.jsx'
 export const App = ()=>{
-    const users = [
-        {
-            name: "Azima",
-            email: "azima@example.com",
-            country: "Uzbekistan"
-        },
-        {
-            name: 'Lili',
-            email: "lili@example.com",
-            country: "Canada"
-        },
-        {
-            name: 'KimSophiya',
-            email: 'kimsophiya@example.com',
-            country: 'South Korea'
-        }
-    ]
-    return (
+    return(
         <main>
-            {users.map((user)=>(
-                <div key={user.email}>
-                    <p>Name is :{user.name}</p>
-                    <p>Email : {user.email}</p>
-                    <p> Country :{user.country}</p>
-                    <hr/>
-                    <UserList/>
-                </div>
-            ))}
+            <Person name={'Azima'} age={17}/>
         </main>
     )
 }
