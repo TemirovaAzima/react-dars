f96e8cf8fe31c363bada6bb1b97b4aabd27860c1
TemirovaAzima <temirovaazima9@gmail.com>
2024-12-07 01:03:23 +0500

    List end

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,33 @@
-import React from 'react'
-import Salomlashish from './components/Salomlashish.jsx'
-import  ProductInfo from './components/ProductInfo.jsx'
-function App(){
-
-    return(
-        <div>
-            <Salomlashish/>
-            <ProductInfo/>
-        </div>
+import {UserList} from "./components/UserList.jsx";
+export const App = ()=>{
+    const users = [
+        {
+            name: "Azima",
+            email: "azima@example.com",
+            country: "Uzbekistan"
+        },
+        {
+            name: 'Lili',
+            email: "lili@example.com",
+            country: "Canada"
+        },
+        {
+            name: 'KimSophiya',
+            email: 'kimsophiya@example.com',
+            country: 'South Korea'
+        }
+    ]
+    return (
+        <main>
+            {users.map((user)=>(
+                <div key={user.email}>
+                    <p>Name is :{user.name}</p>
+                    <p>Email : {user.email}</p>
+                    <p> Country :{user.country}</p>
+                    <hr/>
+                    <UserList/>
+                </div>
+            ))}
+        </main>
     )
 }
-export default App
